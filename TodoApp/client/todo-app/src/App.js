d5ac1dbd0e72d42f1b0504e9ee3da36752ac1860
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-03 00:36:04 +0200

    Update App.js

--- TodoApp/client/todo-app/src/App.js
+++ TodoApp/client/todo-app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer.js";
+import { TodoList } from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main className="main">
+        <section className="todo-list-container">
+          <h1>Todo List</h1>
+
+          <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+          </div>
+
+          <div className="table-wrapper">
+
+            <TodoList />
+            
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
