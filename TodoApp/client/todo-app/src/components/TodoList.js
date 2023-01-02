77509360a828b71b5ecc753047e443db243d871b
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-03 01:02:23 +0200

    add fetch data functionality

--- TodoApp/client/todo-app/src/components/TodoList.js
+++ TodoApp/client/todo-app/src/components/TodoList.js
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
+
+// import { Spinner } from "./Spinner";
 import { TodoItem } from "./TodoItem";
 
 export const TodoList = () => {
-    
+  const [todoList, setList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/jsonstore/todos")
+      .then((response) => response.json())
+      .then((result) => setList(Object.values(result)));
+  }, []);
+
   return (
     <table className="table">
       <thead>
@@ -12,9 +22,9 @@ export const TodoList = () => {
         </tr>
       </thead>
       <tbody>
-
-        <TodoItem />
-
+        {todoList.map((t) => (
+          <TodoItem key={t._id} {...t} />
+        ))}
       </tbody>
     </table>
   );
