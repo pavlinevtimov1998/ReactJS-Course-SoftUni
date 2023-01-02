c43617cff177aa58f19cc67133a59e61235d8674
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-03 01:02:04 +0200

    Update App.js

--- TodoApp/client/todo-app/src/App.js
+++ TodoApp/client/todo-app/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer.js";
+import { Footer } from "./components/Footer";
 import { TodoList } from "./components/TodoList";
 
 function App() {
