cf232d50f5dce3f5869bcd611ac5e0c6644cfd9c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 20:56:05 +0200

    add game home item

---
+++ GamesPlay/client/games-play/src/components/common/GameHomeItem.js
@@ -0,0 +1,22 @@
+export const GameHomeItem = () => {
+  return (
+    <div class="game">
+      <div class="image-wrap">
+        <img src="./images/CoverFire.png" />
+      </div>
+      <h3>Cover Fire</h3>
+      <div class="rating">
+        <span>☆</span>
+        <span>☆</span>
+        <span>☆</span>
+        <span>☆</span>
+        <span>☆</span>
+      </div>
+      <div class="data-buttons">
+        <a href="#" class="btn details-btn">
+          Details
+        </a>
+      </div>
+    </div>
+  );
+};
--- GamesPlay/client/games-play/src/components/common/Home.js
+++ GamesPlay/client/games-play/src/components/common/Home.js
@@ -1,3 +1,5 @@
+import { GameHomeItem } from "./GameHomeItem";
+
 export const Home = () => {
   return (
     <section id="welcome-world">
@@ -11,60 +13,7 @@ export const Home = () => {
         <h1>Latest Games</h1>
 
         {/* <!-- Display div: with information about every game (if any) --> */}
-        {/* <div class="game">
-          <div class="image-wrap">
-            <img src="./images/CoverFire.png" />
-          </div>
-          <h3>Cover Fire</h3>
-          <div class="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div class="data-buttons">
-            <a href="#" class="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
-        <div class="game">
-          <div class="image-wrap">
-            <img src="./images/ZombieLang.png" />
-          </div>
-          <h3>Zombie Lang</h3>
-          <div class="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div class="data-buttons">
-            <a href="#" class="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
-        <div class="game">
-          <div class="image-wrap">
-            <img src="./images/MineCraft.png" />
-          </div>
-          <h3>MineCraft</h3>
-          <div class="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div class="data-buttons">
-            <Link to="" className="btn details-btn">
-              Details
-            </Link>
-          </div>
-        </div> */}
+        <GameHomeItem />
 
         {/* <!-- Display paragraph: If there is no games  --> */}
         <p class="no-articles">No games yet</p>
