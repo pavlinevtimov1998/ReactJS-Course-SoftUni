ed8df759403ab2da97fcbf932c3f6bedd267abae
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 20:12:08 +0200

    add header

--- GamesPlay/client/games-play/src/App.js
+++ GamesPlay/client/games-play/src/App.js
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
+import { Header } from "./components/Header";
+
 function App() {
   return (
     <div id="box">
+      <Header />
+
       <main id="main-content">
         <Routes>
           <Route path="/" element=""></Route>
