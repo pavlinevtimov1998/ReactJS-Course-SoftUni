12f7d94f783a789dec3512b6580c93964e85b8dc
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 20:56:15 +0200

    add game catalog item

---
+++ GamesPlay/client/games-play/src/components/GameCatalogItem.js
@@ -0,0 +1,14 @@
+export const GameCatalogItem = () => {
+  return (
+    <div className="allGames">
+      <div className="allGames-info">
+        <img src="./images/avatar-1.jpg" />
+        <h6>Action</h6>
+        <h2>Cover Fire</h2>
+        <a href="#" className="details-button">
+          Details
+        </a>
+      </div>
+    </div>
+  );
+};
