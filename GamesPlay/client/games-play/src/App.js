b7d095ebd1b2cf66f670432f356b5f61e28c73e0
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 20:31:59 +0200

    add other paths

--- GamesPlay/client/games-play/src/App.js
+++ GamesPlay/client/games-play/src/App.js
@@ -2,6 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import { Header } from "./components/common/Header";
 import { Home } from "./components/common/Home";
+import { Details } from "./components/Details";
+import { Catalog } from "./components/Catalog";
+import { Create } from "./components/Create";
+import { Edit } from "./components/Edit";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register";
 
 function App() {
   return (
@@ -11,8 +17,15 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/register" element={<Home />}></Route>
-          <Route path="/login" element={<Home />}></Route>
+
+          <Route path="/catalog" element={<Catalog />}></Route>
+          <Route path="/details/:gameId" element={<Details />}></Route>
+
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/edit/:gameId" element={<Edit />}></Route>
+
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </main>
     </div>
