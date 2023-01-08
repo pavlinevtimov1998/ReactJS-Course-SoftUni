f2e47a9cc03cd39afaaaa88160660f42b5d2c476
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 23:01:38 +0200

    Update userService.js

--- GamesPlay/client/games-play/src/services/userService.js
+++ GamesPlay/client/games-play/src/services/userService.js
@@ -3,8 +3,11 @@ import * as api from "./api";
 const endpoinds = {
   login: "/users/login",
   register: "/users/register",
+  logout: "/users/logout",
 };
 
 export const login = (data) => api.postRequest(endpoinds.login, data);
 
 export const register = (data) => api.postRequest(endpoinds.register, data);
+
+export const logout = (token) => api.getRequest(endpoinds.logout, token);
