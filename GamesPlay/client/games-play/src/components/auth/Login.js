a7efac30563d07688a12a9bdfb19fb0eec2a1186
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 22:10:26 +0200

    add login functionality

--- GamesPlay/client/games-play/src/components/auth/Login.js
+++ GamesPlay/client/games-play/src/components/auth/Login.js
@@ -1,9 +1,34 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+
+import { login } from "../../services/userService";
 
 export const Login = () => {
+  const { handleLogin } = useContext(AuthContext);
+  const navigate = useNavigate("/");
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setData((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    login(data).then((result) => {
+      handleLogin(result);
+      navigate("/", { replace: true });
+    });
+  };
+
   return (
     <section id="login-page" className="auth">
-      <form id="login">
+      <form id="login" onSubmit={loginHandler}>
         <div className="container">
           <div className="brand-logo" />
           <h1>Login</h1>
@@ -13,9 +38,17 @@ export const Login = () => {
             id="email"
             name="email"
             placeholder="Sokka@gmail.com"
+            value={data.username}
+            onChange={onChangeHandler}
           />
           <label htmlFor="login-pass">Password:</label>
-          <input type="password" id="login-password" name="password" />
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            value={data.password}
+            onChange={onChangeHandler}
+          />
           <input type="submit" className="btn submit" defaultValue="Login" />
           <p className="field">
             <span>
