4d5f83a66552843d52d633ac62aef1a286d9ea04
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 23:38:44 +0200

    add create game function

--- GamesPlay/client/games-play/src/services/gameService.js
+++ GamesPlay/client/games-play/src/services/gameService.js
@@ -3,8 +3,11 @@ import * as api from "./api";
 const endpoints = {
   homeGames: "/data/games?sortBy=_createdOn%20desc&distinct=category",
   catalogGames: "/data/games?sortBy=_createdOn%20desc",
+  create: "/data/games",
 };
 
 export const getHomeGames = () => api.getRequest(endpoints.homeGames);
 
 export const getCatalogGames = () => api.getRequest(endpoints.catalogGames);
+
+export const createGame = (data, token) => api.postRequest(data, token);
