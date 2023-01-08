65f1996f013acc84794643174f8eaa94f887f709
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-08 22:10:50 +0200

    add paths statement logic

--- GamesPlay/client/games-play/src/components/common/Header.js
+++ GamesPlay/client/games-play/src/components/common/Header.js
@@ -1,6 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../../contexts/AuthContext";
+
 export const Header = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <header>
       <h1>
@@ -10,16 +15,17 @@ export const Header = () => {
       </h1>
       <nav>
         <Link to="/catalog">All games</Link>
-        {/* <!-- Logged-in users --> */}
-        <div id="user">
-          <Link to="/create">Create Game</Link>
-          <Link to="/logout">Logout</Link>
-        </div>
-        {/* <!-- Guest users --> */}
-        <div id="guest">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
+        {user ? (
+          <div id="user">
+            <Link to="/create">Create Game</Link>
+            <Link to="/logout">Logout</Link>
+          </div>
+        ) : (
+          <div id="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
       </nav>
     </header>
   );
